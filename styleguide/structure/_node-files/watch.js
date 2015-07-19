f2fed70779b2e3d95ac0036e744ce66595213920
Caio Vaccaro <caiovaccaromora@gmail.com>
2015-07-19 00:17:56 -0300

    Update watch.js

--- styleguide/structure/_node-files/watch.js
+++ styleguide/structure/_node-files/watch.js
@@ -13,7 +13,7 @@ watcher.start();
 livereloader.start();
 
 // Initialize Harp
-exec('cd ' + utils.basePath + '&& harp server', utils.puts);
+exec('cd ' + utils.basePath + '&& harp server --port ' + PORT, utils.puts);
 exec('echo "Starting Server.." && echo "PROGRESS:94"', utils.puts);
 exec('sleep 6 "Almost there!" && "PROGRESS:98"', utils.puts);
 exec('sleep 15 && echo "Done! Enjoy!" && echo "PROGRESS:100" && open "http://localhost:' + PORT + '"', utils.puts);

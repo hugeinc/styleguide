06ba8df0003957b6137fde2477fe78bf759f2706
Caio Vaccaro <caiovaccaromora@gmail.com>
2015-07-21 07:40:48 -0300

    Wait for HTTP 200 response.

--- styleguide/structure/_node-files/watch.js
+++ styleguide/structure/_node-files/watch.js
@@ -5,7 +5,30 @@ var exec = require('child_process').exec,
 	watcher = require('./modules/watcher'),
 	livereloader = require('./modules/livereloader'),
 	utils = require('./modules/utils'),
+	http = require('http'),
+	waitingTheServer = true,
 	PORT = 9000;
+
+function waitTheServer() {
+	if(waitingTheServer) {
+		http.get({
+			host: 'localhost',
+			port: PORT,
+			path: '/'
+		}, function(res) {
+			if(res.statusCode == 200) {
+				exec('echo "Done! Enjoy!" && echo "PROGRESS:100" && open "http://localhost:' + PORT + '"', utils.puts);
+				waitingTheServer = false;
+			}
+			res.emit('end');
+			waitTheServer();
+		}).on('error', function(e) {
+			// Nothing
+			waitingTheServer = true;
+			waitTheServer();
+		});
+	}
+}
 
 // Files watcher
 watcher.start();
@@ -15,5 +38,4 @@ livereloader.start();
 // Initialize Harp
 exec('cd ' + utils.basePath + '&& harp server --port ' + PORT, utils.puts);
 exec('echo "Starting Server.." && echo "PROGRESS:94"', utils.puts);
-exec('sleep 6 "Almost there!" && "PROGRESS:98"', utils.puts);
-exec('sleep 15 && echo "Done! Enjoy!" && echo "PROGRESS:100" && open "http://localhost:' + PORT + '"', utils.puts);
+waitTheServer();

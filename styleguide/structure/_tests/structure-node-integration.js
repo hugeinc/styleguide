451390d27abd8304b6e063ac841ff3c86c21ea44
Caio Vaccaro <cavaccaro@hugeinc.com>
2015-07-24 10:14:30 -0300

    Beginning Integration test

--- styleguide/structure/_tests/structure-node-integration.js
+++ styleguide/structure/_tests/structure-node-integration.js
@@ -1,1 +1,4 @@
-// TODO: Integration tests
+// TODO: Integration tests, Livereload, writeDate, writeToJavascript, writeToStylesheet
+var test = require('../_node-files/node_modules/tape'),
+    watch = require('../_node-files/watch');
+
